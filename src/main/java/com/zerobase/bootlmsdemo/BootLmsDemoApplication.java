e593daa0897f6f642b2ff24a85c04d8bc0abc216
Woogie95 <choisw950928@gmail.com>
2023-08-11 19:56:17 +0900

    Initial commit

---
+++ src/main/java/com/zerobase/bootlmsdemo/BootLmsDemoApplication.java
@@ -0,0 +1,13 @@
+package com.zerobase.bootlmsdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootLmsDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BootLmsDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zerobase/bootlmsdemo/BootLmsDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zerobase.bootlmsdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootLmsDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
